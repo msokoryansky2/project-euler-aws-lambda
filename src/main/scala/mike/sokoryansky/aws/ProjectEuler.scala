25d4bb162d32d77aac4980e17e754e1a6d00f4db
M S <msokoryansky@yahoo.com>
2017-11-19 20:25:27 -0500

    adding a handler for Project Euler requests

--- src/main/scala/mike/sokoryansky/aws/ProjectEuler.scala
+++ src/main/scala/mike/sokoryansky/aws/ProjectEuler.scala
@@ -8,7 +8,7 @@ import mike.sokoryansky.EulerProblems.EulerProblem
 class ProjectEuler {
   import java.io.{InputStream, OutputStream}
 
-  def greeting(input: InputStream, output: OutputStream): Unit = {
+  def solve(input: InputStream, output: OutputStream): Unit = {
     val problemNumber: Int = new BufferedReader(new InputStreamReader(input)).lines().collect(Collectors.joining("")).toInt
     val answer: String = EulerProblem(problemNumber) match {
       case Some(p) => p.run
