96dac0bf43cbbcd072df88fc26418136969b84bd
M S <msokoryansky@yahoo.com>
2017-11-19 20:25:08 -0500

    adding a handler for Project Euler requests

--- src/main/scala/mike/sokoryansky/aws/Hello.scala
+++ src/main/scala/mike/sokoryansky/aws/Hello.scala
@@ -1,10 +1,14 @@
 package mike.sokoryansky.aws
 
+import java.io.{BufferedReader, InputStreamReader}
+import java.util.stream.Collectors
+
 class Hello {
   import java.io.{InputStream, OutputStream}
 
   def greeting(input: InputStream, output: OutputStream): Unit = {
-    val result = s"Yo!"
+    val json: String = new BufferedReader(new InputStreamReader(input)).lines().collect(Collectors.joining("\n"))
+    val result = s"Yo! I got this: $json"
     output.write(result.getBytes("UTF-8"))
   }
 }
---
+++ src/main/scala/mike/sokoryansky/aws/ProjectEuler.scala
@@ -0,0 +1,19 @@
+package mike.sokoryansky.aws
+
+import java.io.{BufferedReader, InputStreamReader}
+import java.util.stream.Collectors
+
+import mike.sokoryansky.EulerProblems.EulerProblem
+
+class ProjectEuler {
+  import java.io.{InputStream, OutputStream}
+
+  def greeting(input: InputStream, output: OutputStream): Unit = {
+    val problemNumber: Int = new BufferedReader(new InputStreamReader(input)).lines().collect(Collectors.joining("")).toInt
+    val answer: String = EulerProblem(problemNumber) match {
+      case Some(p) => p.run
+      case _ => "N/A"
+    }
+    output.write(answer.getBytes("UTF-8"))
+  }
+}
