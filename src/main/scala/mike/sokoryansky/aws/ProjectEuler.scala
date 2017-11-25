e08e28b733002b44c9dbaed3166c74e8ac100c0f
M S <msokoryansky@yahoo.com>
2017-11-24 19:03:43 -0500

    output JSON instead of a string

--- src/main/scala/mike/sokoryansky/aws/ProjectEuler.scala
+++ src/main/scala/mike/sokoryansky/aws/ProjectEuler.scala
@@ -11,7 +11,7 @@ class ProjectEuler {
   def solve(input: InputStream, output: OutputStream): Unit = {
     val problemNumber: Int = new BufferedReader(new InputStreamReader(input)).lines().collect(Collectors.joining("")).toInt
     val answer: String = EulerProblem(problemNumber) match {
-      case Some(p) => p.run
+      case Some(p) => "{ \"number\" : \"" + problemNumber.toString + "\", \"answer\": \"" + p.run + "\" }"
       case _ => "N/A"
     }
     output.write(answer.getBytes("UTF-8"))
